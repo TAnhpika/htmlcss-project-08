62ad45fa38b62daa71e1b15e440bd311f34d8da5
TAnhpika <anhsheeran12345@gmail.com>
2025-11-19 11:33:54 +0700

    User menu #2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -221,3 +221,22 @@ window.addEventListener("template-loaded", () => {
         });
     });
 });
+
+// Switch theme
+//add id= switch-theme-btn + wrap 'Dark mode' with span
+window.addEventListener("template-loaded", () => {
+    const switchBtn = document.querySelector("#switch-theme-btn");
+    if (switchBtn) {
+        switchBtn.onclick = function () {
+            const isDark = localStorage.dark === "true";
+            document.querySelector("html").classList.toggle("dark", !isDark);
+            localStorage.setItem("dark", !isDark);
+            switchBtn.querySelector("span").textContent = isDark ? "Dark mode" : "Light mode";
+        };
+        const isDark = localStorage.dark === "true";
+        switchBtn.querySelector("span").textContent = isDark ? "Light mode" : "Dark mode";
+    }
+});
+
+const isDark = localStorage.dark === "true";
+document.querySelector("html").classList.toggle("dark", isDark);
